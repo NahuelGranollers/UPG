738e1a2f3dce6cbbd8f0ae554704a4cea0cb85ae
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-11-19 21:45:52 +0100

    tail

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
