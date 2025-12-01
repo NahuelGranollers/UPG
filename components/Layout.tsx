c239c8acc3c6228881e1042d741c80d8b845e2d2
JuegosWeb <gd6qs6gqv9@privaterelay.appleid.com>
2025-12-01 20:48:27 +0100

    refactor: Simplify Impostor component and remove unused imports from Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { useSocket } from '../context/SocketContext';
 import { useUsers } from '../context/UserContext';
-import { useVoice } from '../hooks/useVoice';
 import Sidebar from './Sidebar';
 import MobileSidebar from './MobileSidebar';
 import { UserRole } from '../types';
